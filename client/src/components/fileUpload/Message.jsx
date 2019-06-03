6a981967fc2b7933aff285d04897b6e5bbeb8b8f
michaelanggriawan <michaelanggriawan941@gmail.com>
2019-06-04 02:37:48 +0700

    first commit

---
+++ client/src/components/fileUpload/Message.jsx
@@ -0,0 +1,26 @@
+    
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
+
+const Message = ({ msg}) => {
+  const [ hidden, setHidden ] = useState(false);
+  return (
+    <div className='alert alert-info alert-dismissible fade show' role='alert' >
+      {msg}
+      <button
+        type='button'
+        className='close'
+        data-dismiss='alert'
+        aria-label='close'
+      >
+        <span aria-hidden='true' >&times;</span>
+      </button>
+    </div>
+  );
+};
+
+Message.propTypes = {
+  msg: PropTypes.string.isRequired,
+};
+
+export default Message;
